4d6a31312f9caaa8b5579a2ed3bdf9ad5485e474
Illia Antonenko <iantonenko@campmanagement.com>
2026-01-04 20:29:51 +0200

    Use ShouldBroadcastNow to bypass queue for real-time updates

--- app/Events/AttendanceRegistered.php
+++ app/Events/AttendanceRegistered.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\EventRegistration;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class AttendanceRegistered implements ShouldBroadcast
+class AttendanceRegistered implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
